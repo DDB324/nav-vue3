3e12fa14be694f5b0156ec0419986c330943f8a8
ddb324 <jiangdaoran@163.com>
2020-12-13 16:52:11 +0800

    初始化vue-router

---
+++ src/components/NavPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>ddb的导航网站</div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import {createApp} from 'vue';
+import App from './App.vue';
+import NavPage from './components/NavPage.vue';
+import './index.css';
+import {createWebHashHistory, createRouter} from 'vue-router';
+
+const history = createWebHashHistory();
+const router = createRouter({
+  history: history,
+  routes: [
+    {path: '/', component: NavPage}
+  ]
+});
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
